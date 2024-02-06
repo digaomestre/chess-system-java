ee31a5c36e69398bb97d4f00ff79365c35bdcda1
Rodrigo Tomaz <rodrigo_souza2013@yahoo.com.br>
2024-02-05 21:27:17 -0300

    Possible moves of a piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,16 +1,35 @@
 package boardgame;
 
-public class Piece {
-	
+public abstract class Piece {
+
 	protected Position position;
 	private Board board;
-	
-	public Piece (Board board) {
+
+	public Piece(Board board) {
 		this.board = board;
 	}
 
 	public Board getBoard() {
 		return board;
 	}
-	
+
+	public abstract boolean[][] possibleMoves();
+
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+		for (int i = 0; i < mat.length; i++) {
+			for (int x = 0; x < mat.length; x++) {
+				if (mat[i][x]) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -49,6 +49,9 @@ public class ChessMatch {
 		if(!board.thereIsAPiece(source)) {
 			throw new ChessException("There is no piece on source position.");
 		}
+		if(!board.getPiece(source).isThereAnyPossibleMove()) {
+			throw new ChessException("There is no possible moves for the chosen piece");
+		}
 	}
 
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
